bc37c280edb39c3bda118b241fe8c5013a047d30
Mariel Salcedo <mjjsalcedo@yahoo.com>
2017-08-24 23:46:27 -1000

    able to render users to select players page

---
+++ src/containers/selectPlayer/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import store from '../../store.js'
+
+class SelectPlayer extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    var currentUsers = store.getState()
+    var result = currentUsers.messages.filter(users=>{
+      if(users.id !== localStorage.id){
+        return users.username;
+      }
+    })
+
+    return (
+    <div className="allusers">
+    <h2> Welcome {localStorage.username}, choose your partner! </h2>
+    <h2> Current Users </h2>
+       <div className="userList">
+          <p className="users">{result}</p>
+      </div>
+    </div>
+      )
+  }
+}
+
+const mapStateToProps = (state) => {
+  console.log('testselect', state)
+  return {
+    messages: state
+  }
+
+}
+
+export default SelectPlayer;
