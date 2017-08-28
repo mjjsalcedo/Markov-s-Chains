22855d8e2767932ebe3106fce72c556aa5311456
Mariel Salcedo <mjjsalcedo@yahoo.com>
2017-08-27 17:01:33 -1000

    can render different componenets based on whether user is player 1/2

---
+++ src/containers/roomContainer/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import CellPhoneContainer from '../cellPhoneContainer'
+import GraphicsContainer from '../graphicsContainer'
+import StoryContainer from '../storyContainer'
+import InventoryContainer from '../inventoryContainer'
+
+class RoomContainer extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+
+    };
+
+  }
+  render() {
+    return (
+       <div className="mainContainer">
+  <div className="visualContainer">
+  <GraphicsContainer/>
+  <InventoryContainer/>
+  <StoryContainer/>
+  </div>
+  <CellPhoneContainer/>
+  </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    player1: state.player1,
+    player2: state.player2
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+
+  }
+}
+
+const Room = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(RoomContainer);
+
+export default Room;
