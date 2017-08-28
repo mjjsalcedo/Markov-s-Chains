e1cd788d9d5af5d8d6502f47876c0db7da598b6a
Mariel Salcedo <mjjsalcedo@yahoo.com>
2017-08-28 13:58:10 -1000

    revised css and updated image for phone container

--- src/containers/graphicsContainer/index.js
+++ src/containers/graphicsContainer/index.js
@@ -7,17 +7,7 @@ render(){
   <div className="graphicsContainerBorder">
   {
     ( localStorage.getItem("player") === "player1" ) ?
-      <div className="graphicsContainer">Player 1 Graphics go here</div>
-            : null
-          }
-
-    {
-    ( localStorage.getItem("player") === "player2" ) ?
-      <div className="graphicsContainer">Player 2 Graphics go here</div>
-            : null
-          }
-
-        <div className="graphicsContainer">
+      <div className="graphicsContainer"> Player 1
           <div className="key">
           </div>
           <div className="spider">
@@ -45,6 +35,42 @@ render(){
           <div className='rat'>
           </div>
         </div>
+            : null
+          }
+
+    {
+    ( localStorage.getItem("player") === "player2" ) ?
+      <div className="graphicsContainer">Player 2 <div className="key">
+          </div>
+          <div className="spider">
+          </div>
+          <div className='voodoo'>
+          </div>
+          <div className='wand'>
+          </div>
+          <div className='mariel'>
+          </div>
+          <div className='whip'>
+          </div>
+          <div className='painting'>
+          </div>
+          <div className='ian'>
+          </div>
+          <div className='sword'>
+          </div>
+          <div className='torch'>
+          </div>
+          <div className='torch-slant'>
+          </div>
+          <div className='bat'>
+          </div>
+          <div className='rat'>
+          </div>
+        </div>
+            : null
+          }
+
+
     </div>
   )
 }
