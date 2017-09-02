a646cd2f70b4b75b39246c992368b2f7361c210e
Reyn-L <Reyn.Leong@gmail.com>
2017-09-01 23:48:56 -1000

    setting up server

--- src/containers/usernamePage/index.js
+++ src/containers/usernamePage/index.js
@@ -1,80 +1,77 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { createUsername, userConnect } from '../../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { createUsername, userConnect } from "../../actions";
 
 class UserLogin extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
+      username: "",
       id: localStorage.getItem("id")
-    }
+    };
   }
 
-  handleCreateUsername(e){
-    e.preventDefault()
-    this.props.createUsername(this.state)
-    this.setState({ username: '' })
-    this.props.history.push('/userlist')
+  handleCreateUsername(e) {
+    e.preventDefault();
+    this.props.createUsername(this.state);
+    this.setState({ username: "" });
+    this.props.history.push("/userlist");
   }
-  usernameInput(e){
-    this.setState({ username: e.target.value })
+  usernameInput(e) {
+    this.setState({ username: e.target.value });
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.props.userConnect();
   }
 
   render() {
     return (
-      <div className='usernameMainContainer'>
-      <div className='usernameFormBorder'>
-      <h1 className='userTitle'> MIRKOV </h1>
-      <div className="usernameFormContainer">
-      <div className='userMariel'>
+      <div className="usernameMainContainer">
+        <div className="usernameFormBorder">
+          <h1 className="userTitle"> MIRKOV </h1>
+          <div className="usernameFormContainer">
+            <div className="userMariel" />
+            <div className="userIan" />
+            <div className="userReyn" />
+            <div className="sleeping">z</div>
+            <div className="sleeping2">z</div>
+            <div className="sleeping3">Z</div>
+          </div>
+          <input
+            className="userChatInput"
+            placeholder="input your username"
+            value={this.state.username}
+            onChange={this.usernameInput.bind(this)}
+          />
+          <button
+            type="submit"
+            className="userSendButton btn"
+            onClick={this.handleCreateUsername.bind(this)}
+          >
+            Submit
+          </button>
         </div>
-        <div className='userIan'>
-        </div>
-        <div className='userReyn'>
-        </div>
-        <div className="sleeping">
-        z
       </div>
-      <div className="sleeping2">
-      z
-      </div>
-      <div className='sleeping3'>
-      Z
-      </div>
-      </div>
-      <input className="userChatInput" placeholder="input your username" value={ this.state.username } onChange={ this.usernameInput.bind(this) }>
-      </input>
-      <button type="submit" className="userSendButton btn" onClick={ this.handleCreateUsername.bind(this)}>Submit
-      </button>
-      </div>
-      </div>
-      )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {}
-}
+const mapStateToProps = state => {
+  return {};
+};
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    createUsername: (username) => {
-      dispatch(createUsername(username))
-    } ,
+    createUsername: username => {
+      dispatch(createUsername(username));
+    },
     userConnect: () => {
-      dispatch(userConnect())
+      dispatch(userConnect());
     }
-  }
-}
+  };
+};
 
-UserLogin = connect(
-  mapStateToProps,
-  mapDispatchToProps
-  )(UserLogin)
+UserLogin = connect(mapStateToProps, mapDispatchToProps)(UserLogin);
 
 export default UserLogin;
